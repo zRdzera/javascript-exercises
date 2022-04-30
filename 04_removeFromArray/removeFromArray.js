127d0728555bc4e3ea3d821c595534efd78e99b0
Everton Zardo <evezardo15@gmail.com>
2022-04-30 18:41:23 -0300

    End of remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(element, ...args) {
 
+    // while (count > 0){
+    //     for(let j = 0; j < args.length; j++){
+    //         if(element[count] === args[j]){
+    //             element.splice(count, 1);
+    //             console.log(element.length);
+    //         }
+    //     }
+    //     count--;
+    // } 
+
+    /* Both structures (above and below) work on this exercise */
+
+    for(let i = element.length-1; i >= 0; i--){
+        for(let j = 0; j < args.length; j++){
+            if(element[i] === args[j]){
+                element.splice(i, 1);
+            }
+        }
+    }
+
+    return element;
 };
 
 // Do not edit below this line
