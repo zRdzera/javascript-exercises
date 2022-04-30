7a8152bf7c118d5f1a5e5b86e1d51f5af969764e
Everton Zardo <evezardo15@gmail.com>
2022-04-30 19:22:56 -0300

    End of sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(initialNumber, finalNumber) {
+    if(initialNumber < 0 || finalNumber < 0) return "ERROR";
 
+    if(typeof initialNumber != 'number' || typeof finalNumber != 'number') return "ERROR";
+
+    let sum = 0;
+    
+    if(initialNumber > finalNumber){
+        let aux = initialNumber;
+        initialNumber = finalNumber;
+        finalNumber = aux;
+    }
+    
+    for(let i = initialNumber; i <= finalNumber; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
