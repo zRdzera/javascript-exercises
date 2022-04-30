6ee263cce2e22c173e9136f916e373d77d358365
Everton Zardo <evezardo15@gmail.com>
2022-04-30 20:16:46 -0300

    End of temp conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
+const ftoc = function(fTemp) {
+  let toCelsius;
 
+  toCelsius = (fTemp - 32) * 5/9;
+  toCelsius = +toCelsius.toFixed(1); // Number(toCelsius.toFixed(0)) can be used too.
+
+  return toCelsius;
 };
 
-const ctof = function() {
+const ctof = function(cTemp) {
+  let toFahrenheit;
 
+  toFahrenheit = (cTemp * 1.8) + 32;
+  toFahrenheit = +toFahrenheit.toFixed(1); // Number(toFahrenheit.toFixed(0)) can be used too.
+
+  return toFahrenheit;
 };
 
 // Do not edit below this line
