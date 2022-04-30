0997a42df2406850947973914f05df79adcf965e
Everton Zardo <evezardo15@gmail.com>
2022-04-30 12:59:23 -0300

    End of reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let subString = '';
 
+    for(let i = string.length-1; i >= 0; i --){
+        subString += string.charAt(i);
+    }
+
+    return subString;
 };
 
 // Do not edit below this line
