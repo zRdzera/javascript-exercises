28218e097057352a620a548c6892aab35a0ad9de
Everton Zardo <evezardo15@gmail.com>
2022-04-30 12:15:17 -0300

    End of repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let words = '';
 
+    if(num < 0){
+        return "ERROR";
+    }
+    else {
+        for(let i = 0; i < num; i++){
+            words += string;
+        }
+        return words;
+    }
 };
 
 // Do not edit below this line
