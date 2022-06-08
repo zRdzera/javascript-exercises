4c7b9476c20eea1fc5bd6e9d006251b2a304aada
Everton Zardo <evezardo15@gmail.com>
2022-06-08 02:14:56 -0300

    The add function passed all tests for it

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,6 +1,15 @@
-const add = function() {
-	
+const add = function(...obj) {
+  return obj.reduce((acc, currentValue) => acc + currentValue, 0);
 };
+
+      /* OTHER RESOLUTION */
+// const add = function(...obj) {
+//   const result = obj.reduce((previousValue, currentValue) => {
+//     return previousValue + currentValue;
+//   }, 0);
+
+//   return result;
+// };
 
 const subtract = function() {
 	
--- 08_calculator/calculator.spec.js
+++ 08_calculator/calculator.spec.js
@@ -5,11 +5,11 @@ describe('add', () => {
 		expect(calculator.add(0,0)).toBe(0);
 	});
 
-	test.skip('adds 2 and 2', () => {
+	test('adds 2 and 2', () => {
 		expect(calculator.add(2,2)).toBe(4);
 	});
 
-	test.skip('adds positive numbers', () => {
+	test('adds positive numbers', () => {
 		expect(calculator.add(2,6)).toBe(8);
 	});
 });
