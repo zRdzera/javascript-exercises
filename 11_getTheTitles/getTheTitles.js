6824024646d3530fea7776fae5f70626a04e3a9f
Everton Zardo <evezardo15@gmail.com>
2022-06-12 13:30:21 -0300

    End of getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function(obj) {
+    let array = [];
 
+    for (let index = 0; index < obj.length; index++) {
+        array[index] = obj[index].title;
+    }
+
+    return array;
 };
+
+  /* OTHER WAY TO SOLVE THE EXERCISE */
+
+// const getTheTitles = function(obj) {
+//     return obj.map(book => book.title);
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
