490cb607e7d419046b144239fededaaa7d0bf2cc
Everton Zardo <evezardo15@gmail.com>
2022-06-11 21:38:52 -0300

    End of fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let array = new Array();
+    array[0] = 1;
+    array[1] = 1;
 
+    if (num < 0) return "OOPS";
+
+    for (let index = 2; index < num; index++) {
+        let result = array[index-1] + array[index-2];
+        array[index] = result;
+    }
+
+    return array[num-1];
 };
 
 // Do not edit below this line
