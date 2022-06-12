eaa219300133bfca0394f957f83b22363eb127f6
Everton Zardo <evezardo15@gmail.com>
2022-06-12 15:25:14 -0300

    End of findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    const todayDate = new Date();
+    const thisYear = todayDate.getFullYear();
 
+    const sortArray = obj.sort((element, nextOne) => 
+    {
+        if(!element.yearOfDeath) element.yearOfDeath = thisYear;    
+        if(!nextOne.yearOfDeath) nextOne.yearOfDeath = thisYear;
+
+        return (element.yearOfDeath - element.yearOfBirth) > (nextOne.yearOfDeath - nextOne.yearOfBirth) ? -1 : 1;
+    });
+
+    return sortArray[0];
 };
 
 // Do not edit below this line
