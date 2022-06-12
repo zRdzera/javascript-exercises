cd83c04862ec37d87f3c22da40de011bcfbc75de
Everton Zardo <evezardo15@gmail.com>
2022-06-11 21:49:15 -0300

    End of palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,13 +1,10 @@
 const palindromes = function (word) {
-    word = word.replace(/[^a-zA-Z]/g, '');
+    word = word.toLowerCase().replace(/[^a-zA-Z]/g, '');
 
     let newWord = '';
     for (let index = word.length - 1; index >= 0; index--) {
         newWord += word[index];
     }
-
-    newWord = newWord.toLowerCase();
-    word = word.toLowerCase();
 
     return newWord === word ? true : false;
 };
